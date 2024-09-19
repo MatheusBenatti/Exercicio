04e1e2ea83b515e85b098d53ba1a7f621e9ae75a
Matheus Benatti <theusbenatti@gmail.com>
2024-09-19 17:57:41 -0300

    feat: Questao 2

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,18 +1,19 @@
-﻿using Newtonsoft.Json;
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http.Json;
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         string teamName = "Paris Saint-Germain";
         int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
+        int totalGoals = await getTotalScoredGoals(teamName, year);
 
-        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
         teamName = "Chelsea";
         year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+        totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
@@ -21,10 +22,37 @@ public class Program
         // Team Chelsea scored 92 goals in 2014
     }
 
-    public static int getTotalScoredGoals(string team, int year)
+    public static async Task<int> getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
-    }
+        using HttpClient client = new();
+        int totalGoals = 0;
+        int page = 1;
 
+        while (true)
+        {
+            string url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={page}";
+            var response = await client.GetStringAsync(url);
+            
+            JObject data = JObject.Parse(response);
+
+            foreach (var match in data["data"])
+            {
+                totalGoals += (int)match["team1goals"];
+            }
+
+            url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team2={team}&page={page}";
+            response = await client.GetStringAsync(url);
+            data = JObject.Parse(response);
+
+            foreach (var match in data["data"])
+            {
+                totalGoals += (int)match["team2goals"];
+            }
+
+            int totalPages = (int)data["total_pages"];
+            if (page >= totalPages) break;
+            page++;
+        }
+        return totalGoals;
+    }
 }
